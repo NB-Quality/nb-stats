60a5ba4d5c7ab8fd19ab8ebd561300be15200f55
negbook <negbook@gmail.com>
2022-06-19 03:27:27 +0800

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,6 +1,4 @@
 CREATE TABLE `stats` (
-	`license` VARCHAR(200) NOT NULL DEFAULT '0' COLLATE 'utf8mb4_general_ci'
-)
-COLLATE='utf8mb4_general_ci'
-ENGINE=InnoDB
-;
+  `license` VARCHAR(200) NOT NULL DEFAULT '0' COLLATE 'utf8mb4_general_ci',
+  PRIMARY KEY (`license`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
