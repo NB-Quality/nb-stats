6b6f6013f185d84b5d67a2096c2741cea6ce888d
negbook <negbook@gmail.com>
2022-06-20 16:00:53 +0800

    update

--- csv.lua
+++ csv.lua
@@ -1,3 +1,4 @@
+--http://lua-users.org/wiki/CsvUtils
 local function fromLine(s) -- Convert from CSV string to table (converts a single line of a CSV file)
   s = s .. ','        -- ending comma
   local t = {}        -- table to collect fields
@@ -24,6 +25,18 @@ local function fromLine(s) -- Convert from CSV string to table (converts a singl
   return t
 end
 
+local function split (inputstr, sep)
+   if sep == nil then
+           sep = "%s"
+   end
+   local t={}
+   for str in string.gmatch(inputstr, "([^"..sep.."]+)") do
+           table.insert(t, str)
+   end
+   return t
+end
+
+
 ReadCSV = function(path,translationkeys)
     local ln = 1
     local keys = translationkeys or {}
@@ -33,15 +46,58 @@ ReadCSV = function(path,translationkeys)
     local rawdata = rawdata:gsub("\r","")
     local delimiter = "\n"
     for line in (rawdata..delimiter):gmatch('(.-)'..delimiter) do
-        local s = line 
+        local isdescription = line:find("#")
+
         local delimiter = ","
         if ln == 1 then 
+            local s = isdescription and line:gsub("#","") or line 
             if not translationkeys then
                 for match in (s..delimiter):gmatch('(.-)'..delimiter) do
                     table.insert(keys, match);
                 end
             end 
         else 
+            local s = isdescription and split(line,"#")[1] or line 
+            k = 1
+            local linedata = fromLine(line)
+            for i=1,#linedata do
+                local v = linedata[i]
+                if not data[ln-1] then data[ln-1] = {} end 
+                local isNumber = tostring(tonumber(v)) == v 
+                data[ln-1][keys[k]] = isNumber and tonumber(v) or v 
+                k = k + 1
+            end
+        end 
+        ln = ln + 1
+    end
+    --for line in io.lines(GetResourcePath(GetCurrentResourceName()).."/"..path) do
+        
+    --end
+    ln = 1
+    return data
+end 
+
+ReadCSVRaw = function(raw,translationkeys)
+    local ln = 1
+    local keys = translationkeys or {}
+    local k = 1 
+    local data = {}
+    local rawdata = raw
+    local rawdata = rawdata:gsub("\r","")
+    local delimiter = "\n"
+    for line in (rawdata..delimiter):gmatch('(.-)'..delimiter) do
+        local isdescription = line:find("#")
+        
+        local delimiter = ","
+        if ln == 1 then 
+            local s = isdescription and line:gsub("#","") or line 
+            if not translationkeys then
+                for match in (s..delimiter):gmatch('(.-)'..delimiter) do
+                    table.insert(keys, match);
+                end
+            end 
+        else 
+            local s = isdescription and split(line,"#")[1] or line 
             k = 1
             local linedata = fromLine(line)
             for i=1,#linedata do
